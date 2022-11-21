8332359e178f8235960b2bed5a3ad58ac8a6e179
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2022-11-21 10:28:39 +0530

    keyword

---
+++ 20 Nov/function.js
@@ -0,0 +1,62 @@
+
+// let number = 9;
+
+// for (let i = 0; i < 9; i++) {
+
+
+//     let number = 9;
+// }
+
+// let number = 9
+
+
+// var a;
+// let b;
+// let c = 10;
+// c = 12;
+
+
+// console.log(c)
+
+
+
+// let num = -5;
+// let str = '8932';
+
+// let bool = false;
+// console.log(true)
+
+
+
+// console.log(typeof (str));
+
+
+// obj
+
+// let obj = {
+//     name: "aaditya",
+//     age: 34,
+//     num: 8943,
+// };
+
+// let arr = [1, 2, 3, 503, 9];
+// console.log(arr);
+// console.log(arr[3])
+
+
+// console.log(typeof(obj))
+
+// Q. find all the value of a and b using arithmatic operator where a = 23 and b = 24;
+
+// Q. what is data and information ?
+
+// Q. what are different types of data. explain it with example. // show me also it's data types using typeof operator
+
+// Q. what is an array ?
+
+// Q . in js is everything a object? explain it with reason.
+
+
+
+
+
